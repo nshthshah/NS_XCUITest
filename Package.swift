c530d6a8e8be9051a1ba61a300efc1a52c46018a
Nishith Shah <nishith.shah-cw@odhsolutions.com>
2022-11-30 15:02:04 -0500

    init

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NS_XCUITest",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "NS_XCUITestTests",
             dependencies: ["NS_XCUITest"]),
